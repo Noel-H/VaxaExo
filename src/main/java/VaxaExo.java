32f62084dbd45a3d7d9f8773255e4bf9798ee5e3
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-06-16 15:41:01 +0200

    exo5 done

--- src/main/java/VaxaExo.java
+++ src/main/java/VaxaExo.java
@@ -1,7 +1,36 @@
 public class VaxaExo {
     public static void main(String[] args) {
-        int nombreDeZeny = 3;
+        int nombreDeZeny = 0;
         String prenomDeLInterlocuteur = "Peter";
-        System.out.println("Voilà vos " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
+//        System.out.println("Voilà vos " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
+
+        if (nombreDeZeny < 1) {
+            System.out.println("...");
+        }
+
+        if (nombreDeZeny == 1) {
+            System.out.println("Voilà ton zeny, du balai !");
+        }
+
+        if (nombreDeZeny > 1 && nombreDeZeny <= 1_000) {
+            System.out.println("Voilà voz " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
+        }
+
+        if (nombreDeZeny > 1_000 && nombreDeZeny <= 100_000) {
+            System.out.println("Voilà voz " + nombreDeZeny + "z, cher " + prenomDeLInterlocuteur + ". Au plaisir de vous revoir bientôt !");
+        }
+
+        if (nombreDeZeny > 100_000) {
+            System.out.printf("Voilà vos " + nombreDeZeny + "z, Maître " + prenomDeLInterlocuteur + ". Laissez moi vous remercier à la hauteur de votre statut : " + afficheMerci(nombreDeZeny) + "!");
+        }
+    }
+
+    private static String afficheMerci(int nombreDeZeny) {
+       int nombreDeMerci = nombreDeZeny/100_000;
+       String reponse = "";
+        for (int i = 0; i < nombreDeMerci; i++) {
+            reponse = reponse + "Merci ";
+        }
+        return reponse;
     }
 }
