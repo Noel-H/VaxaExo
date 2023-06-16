2d42da028c7026fdc6104e3cfac2f36865ee4730
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-06-16 17:05:19 +0200

    exo6 done

--- src/main/java/VaxaExo.java
+++ src/main/java/VaxaExo.java
@@ -1,36 +1,82 @@
 public class VaxaExo {
     public static void main(String[] args) {
-        int nombreDeZeny = 0;
-        String prenomDeLInterlocuteur = "Peter";
-//        System.out.println("Voilà vos " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
+        vendre(300000, "Peter");
+    }
 
-        if (nombreDeZeny < 1) {
-            System.out.println("...");
+    private static void vendre(int zeny, String nom) {
+        String niveauDeVente = obtenirNiveauDeVente(zeny);
+        reponseDeGary(zeny, nom, niveauDeVente);
+    }
+
+    private static String obtenirNiveauDeVente(int zeny) {
+        if (zeny < 1) {
+            return "NUL";
         }
 
-        if (nombreDeZeny == 1) {
-            System.out.println("Voilà ton zeny, du balai !");
+        if (zeny == 1) {
+            return "MEDIOCRE";
         }
 
-        if (nombreDeZeny > 1 && nombreDeZeny <= 1_000) {
-            System.out.println("Voilà voz " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
+        if (zeny > 1 && zeny <= 1_000) {
+            return "OK";
         }
 
-        if (nombreDeZeny > 1_000 && nombreDeZeny <= 100_000) {
-            System.out.println("Voilà voz " + nombreDeZeny + "z, cher " + prenomDeLInterlocuteur + ". Au plaisir de vous revoir bientôt !");
+        if (zeny > 1_000 && zeny <= 100_000) {
+            return "BON";
         }
 
-        if (nombreDeZeny > 100_000) {
-            System.out.printf("Voilà vos " + nombreDeZeny + "z, Maître " + prenomDeLInterlocuteur + ". Laissez moi vous remercier à la hauteur de votre statut : " + afficheMerci(nombreDeZeny) + "!");
+            return "SUPER";
+    }
+
+    private static void reponseDeGary(int zeny, String nom, String niveauDeVente) {
+        StringBuilder reponse = new StringBuilder();
+
+        if (niveauDeVente.equals("NUL")) {
+            reponse.append(remerciementDeGaryNul());
         }
+
+        if (niveauDeVente.equals("MEDIOCRE")) {
+            reponse.append(remerciementDeGaryMediocre());
+        }
+
+        if (niveauDeVente.equals("OK")) {
+            reponse.append(rendreLaMonnaie(zeny, nom));
+        }
+
+        if (niveauDeVente.equals("BON")) {
+            reponse.append(rendreLaMonnaie(zeny, nom));
+            reponse.append(remerciementDeGaryBon());
+        }
+
+        if (niveauDeVente.equals("SUPER")) {
+            reponse.append(rendreLaMonnaie(zeny, nom));
+            reponse.append(remerciementDeGarySuper(zeny));
+        }
+        System.out.println(reponse);
+    }
+
+    private static String rendreLaMonnaie(int zeny, String nom) {
+        return "Voilà vos " + zeny + "z, cher " + nom;
+    }
+
+    private static String remerciementDeGaryNul() {
+        return "...";
+    }
+
+    private static String remerciementDeGaryMediocre() {
+        return "Voilà ton zeny, du balai !";
+    }
+
+    private static String remerciementDeGaryBon() {
+        return ". Au plaisir de vous revoir bientôt !";
+    }
+
+    private static String remerciementDeGarySuper(int zeny) {
+        return ". Laissez moi vous remercier à la hauteur de votre statut : " + afficheMerci(zeny) + "!";
     }
 
     private static String afficheMerci(int nombreDeZeny) {
        int nombreDeMerci = nombreDeZeny/100_000;
-       String reponse = "";
-        for (int i = 0; i < nombreDeMerci; i++) {
-            reponse = reponse + "Merci ";
-        }
-        return reponse;
+        return "Merci ".repeat(Math.max(0, nombreDeMerci));
     }
 }
