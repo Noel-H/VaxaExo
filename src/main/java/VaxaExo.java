0a186dc391365ae23a2f89d7742e2835b3fecd08
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-06-16 11:07:50 +0200

    exo4 done

--- src/main/java/VaxaExo.java
+++ src/main/java/VaxaExo.java
@@ -1,6 +1,7 @@
 public class VaxaExo {
     public static void main(String[] args) {
         int nombreDeZeny = 3;
-        System.out.println("Voilà vos " + nombreDeZeny + "z");
+        String prenomDeLInterlocuteur = "Peter";
+        System.out.println("Voilà vos " + nombreDeZeny + "z, " + prenomDeLInterlocuteur);
     }
 }
